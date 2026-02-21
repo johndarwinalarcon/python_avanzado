99f959b40fdc548387d078ecec20bd5bc906bab8
johndarwinalarcon <john.darwin.alarcon@gmail.com>
2026-02-21 11:19:40 -0500

    ejercicios de python avanzado

---
+++ paradigma_imperativo.py
@@ -0,0 +1,13 @@
+# Paradigma imperativo
+# aqui damos instrucciones paso a paso 
+
+placas = []  # creamos lista vacia
+placas.append('NZB16E') #agregamos placa
+placas.append('XXX34U') #agregamos otra placa
+
+print('Vehiculos actuales:', placas)    #mostramos lista
+
+placas.remove('NZB16E') # quitamos un vehiculo
+
+print('Despues de salida:', placas) # mostramos resultado
+# le decimos a la computadora que hacer
---
+++ paradigma_procedural.py
@@ -0,0 +1,21 @@
+# PARADIGMA PROCEDURAL
+
+def registrar_entrada(lista, placa):
+    # Agrega un vehiculo a la lista
+    lista.append(placa)
+
+def registrar_salida(lista, placa):
+    # elimina vehiculo si existe
+    if placa in lista:
+        lista.remove(placa)
+
+vehiculos = []
+
+registrar_entrada(vehiculos, 'NZB16E')
+registrar_entrada(vehiculos, 'BBBB23B')
+
+print('vehiculos:', vehiculos)
+
+registrar_salida(vehiculos, 'NZB16E')
+
+print('Despues salida:', vehiculos)
